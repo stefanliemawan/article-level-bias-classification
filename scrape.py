e4034cce03b4e8349ca350973bb733248456a5d6
stefanliemawan <stefanliemawan@gmail.com>
2024-01-15 09:54:18 +0100

    added more outlets

--- scrape.py
+++ scrape.py
@@ -21,11 +21,15 @@ def get_soup(url):
     return soup
 
 
+def scrape_standard(url):
+    ...
+
+
 def scrape_19thnews(url):
     soup = get_soup(url)
     article = soup.find(id="article-data")
 
-    content = article.get_text()
+    content = article.get_text(strip=True)
     content = re.search(
         r"<!-- wp:paragraph -->.+<!-- \\\/wp:paragraph -->", content
     ).group()
@@ -42,7 +46,7 @@ def scrape_rt(url):
     content = ""
     for p in article.find_all("p"):
         if not p.find("strong"):  # for the share story stuff at the bottom
-            p_text = p.get_text()
+            p_text = p.get_text(strip=True)
             content += p_text
 
     return content
@@ -54,9 +58,7 @@ def scrape_newsmax(url):
 
     content = ""
     for p in article.find_all("p"):
-        p_text = p.get_text().replace("\n", "")
-        if "|" in p_text:
-            break
+        p_text = p.get_text(strip=True)
         content += p_text
 
     return content
@@ -68,7 +70,7 @@ def scrape_newsweek(url):
 
     content = ""
     for p in article.find_all("p"):
-        p_text = p.get_text()
+        p_text = p.get_text(strip=True)
         content += p_text
 
     return content
@@ -79,7 +81,71 @@ def scrape_newsy(url):
     soup = get_soup(url)
     article = soup.find(class_="story-transcript")
 
-    content = article.get_text()
+    content = article.get_text(strip=True)
+
+    return content
+
+
+def scrape_nj(url):
+    soup = get_soup(url)
+    article = soup.find(class_="article__story")
+
+    content = ""
+    for p in article.find_all("p"):
+        p_text = p.get_text(strip=True)
+        if (
+            "Our journalism needs your support. Please subscribe today to NJ.com."
+            in p_text
+        ):
+            break
+        content += p_text
+
+    return content
+
+
+def scrape_npr(url):
+    soup = get_soup(url)
+    article = soup.find(class_="storytext")
+
+    content = ""
+    for p in article.find_all("p", class_=None):
+        parent_class = p.find_parent().attrs["class"][0]
+        if parent_class != "caption":
+            p_text = p.get_text(strip=True)
+            content += p_text
+
+    return content
+
+
+def scrape_occupy_democrats(url):
+    soup = get_soup(url)
+    article = soup.find(class_="post-content")
+
+    content = ""
+    for p in article.find_all("p", class_=None):
+        p_text = p.get_text(strip=True)
+        content += p_text
+
+    return content
+
+
+def scrape_palmer_report(url):
+    soup = get_soup(url)
+    article = soup.find(class_="fl-post-content")
+
+    content = ""
+    for p in article.find_all("p", class_=None):
+        p_text = p.get_text(strip=True)
+        content += p_text
+
+    return content
+
+
+def scrape_pbs(url):
+    soup = get_soup(url)
+    article = soup.find(class_="body-text")
+
+    content = article.get_text(strip=True)
 
     return content
 
@@ -104,6 +170,21 @@ def scrape_content(row):
         elif url.startswith("https://www.newsy.com"):
             print(f"scraping {url}...")
             content = scrape_newsy(url)
+        elif url.startswith("https://www.nj.com"):
+            print(f"scraping {url}...")
+            content = scrape_nj(url)
+        elif url.startswith("https://www.npr.org"):
+            print(f"scraping {url}...")
+            content = scrape_npr(url)
+        elif url.startswith("https://occupydemocrats.com"):
+            print(f"scraping {url}...")
+            content = scrape_occupy_democrats(url)
+        elif url.startswith("https://www.palmerreport.com"):
+            print(f"scraping {url}...")
+            content = scrape_palmer_report(url)
+        elif url.startswith("https://www.pbs.org"):
+            print(f"scraping {url}...")
+            content = scrape_pbs(url)
     except Exception as exception:
         print(f"failed, {exception}")
 
@@ -113,11 +194,11 @@ def scrape_content(row):
 df["content"] = df.apply(scrape_content, axis=1)
 print(df.head())
 
-df.to_csv("scraped_3.csv", index=False)
+df.to_csv("scraped_4.csv", index=False)
 
-# scrape_newsy("")
-
-# fix newsmax
+# scrape_newsmax("")
 
 
 # newsnation: This site is currently unavailable to visitors from the European Economic Area while we work to ensure your data is protected in accordance with applicable EU laws.
+# oann: page not found
+# ozy: page not found
