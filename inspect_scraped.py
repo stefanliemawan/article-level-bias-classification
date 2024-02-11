5206626ef6e2d561ea39262cf788d686222d32b7
stefanliemawan <stefanliemawan@gmail.com>
2024-02-11 21:01:19 +0100

    added cleaned df

--- inspect_scraped.py
+++ inspect_scraped.py
@@ -8,11 +8,21 @@ def merge_df():
 
     df[df.isnull()] = scraped_7_df
 
-    df.to_csv("scrape/scraped_merged_uniform4_scraped7.csv")
+    df.to_csv("scrape/scraped_merged.csv")
 
 
-df = pd.read_csv("scrape/scraped_merged_uniform4_scraped7.csv", index_col=0)
-df = df[df["content"].notna()]
+def clean_df():
+    df = pd.read_csv("scrape/scraped_merged.csv", index_col=0)
+    df = df[df["content"].notna()]
 
-print(df)
-# [5407 rows x 8 columns]
+    # invalid content but notna
+    df.drop(df[df["outlet"].str.startswith("hillreporter")].index, inplace=True)
+    df.drop(df[df["outlet"].str.startswith("quillette")].index, inplace=True)
+
+    df.to_csv("scrape/scraped_merged_clean.csv")
+
+    # [5358 rows x 8 columns]
+
+
+# merge_df()
+# clean_df()
