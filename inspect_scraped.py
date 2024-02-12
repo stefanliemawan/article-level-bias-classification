0ca31b0f532aefe102ba4be79f9ca27560c27aaa
stefanliemawan <stefanliemawan@gmail.com>
2024-02-12 19:22:43 +0100

    noise deletion WIP

--- inspect_scraped.py
+++ inspect_scraped.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 
 
@@ -8,21 +9,49 @@ def merge_df():
 
     df[df.isnull()] = scraped_7_df
 
-    df.to_csv("scrape/scraped_merged.csv")
+    df.to_csv("scraped_merged.csv")
 
 
 def clean_df():
-    df = pd.read_csv("scrape/scraped_merged.csv", index_col=0)
+    df = pd.read_csv("scraped_merged.csv", index_col=0)
     df = df[df["content"].notna()]
 
     # invalid content but notna
     df.drop(df[df["outlet"].str.startswith("hillreporter")].index, inplace=True)
     df.drop(df[df["outlet"].str.startswith("quillette")].index, inplace=True)
+    df.drop(df[df["outlet"].str.startswith("www.newsday")].index, inplace=True)
+    df.drop(df[df["outlet"].str.startswith("www.oann")].index, inplace=True)
 
-    df.to_csv("scrape/scraped_merged_clean.csv")
+    df.reset_index(inplace=True)
+    df.to_csv("scraped_merged_clean_v1.csv")
 
     # [5358 rows x 8 columns]
 
 
+def delete_noise(row):
+    content = row["content"].replace("\n", "")
+
+    sentences = content.split(".")
+
+    for index, sentence in enumerate(sentences.copy()):
+        if "Link Copied" in sentence:
+            sentences[index] = sentence[11:]
+        if re.match(r"[Cc]opyright.*\d{4}", sentence):
+            return ". ".join(sentences[:index])
+
+    content = ". ".join(sentences)
+
+    return content
+
+
+def clean_content():
+    df = pd.read_csv("scraped_merged_clean_v1.csv", index_col=0)
+
+    df["content"] = df.apply(delete_noise, axis=1)
+
+    df.to_csv("scraped_merged_clean_v2.csv")
+
+
 # merge_df()
 # clean_df()
+clean_content()
